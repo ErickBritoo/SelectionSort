67ce5610ca48b01cb0369ea0f3087f393fc625e9
Erick Alves <britoerick16@gmail.com>
2025-06-19 18:14:10 -0300

    feat: implementa Selection Sort

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Selection
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[5] { 15, 3, 7, 1, 5};
+            arr = SelectionSort(arr);
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        static int[] SelectionSort(int[] arr) {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int menorElement = arr[i];
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (menorElement < arr[j])
+                    {
+                        int aux = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = aux;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
